e55d47ac8c12c9736e73585ff67a20ecc2c99cf6
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-09-01 10:12:52 +0300

    done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface IComponent {}
 
-  }
+class Component<T extends IComponent> {
+  constructor(public props: T) {}
+}
+interface IPage extends IComponent {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<IPage> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
